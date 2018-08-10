3e1b201c3b3ef3464c74076dbaec9626fff6f730
Vinícius Hoyer <vhoyer@live.com>
2018-08-10 14:49:10 -0300

    Initial Commit

---
+++ My2dContext.js
@@ -0,0 +1,103 @@
+"use strict";
+
+class My2dContext {
+
+    constructor(ctx){
+        this.ctx = ctx;
+        this._zoom = 1;
+
+        let self = this;
+        this.canvas.onmousewheel = function (e) {
+            if (e.deltaY > 0) {
+                self.zoomOut();
+            } else {
+                self.zoomIn();
+            }
+            console.log(self.zoom);
+        };
+    }
+
+    get drawFunction() { return this.draw }
+    set drawFunction(value) { this.draw = value }
+
+    get canvas() { return this.ctx.canvas; }
+    set canvas(value) { this.ctx.canvas = value; }
+
+    get zoom() { return this._zoom; }
+    set zoom(value) {
+        if (value < 0.05){
+            return;
+        }
+        this._zoom = value;
+        this.draw();
+    }
+
+    get w() {
+        return this.canvas.width / this.zoom;
+    }
+
+    get h() {
+        return this.canvas.height / this.zoom;
+    }
+
+    get fillStyle() { return this.ctx.fillStyle; }
+    set fillStyle(value) { this.ctx.fillStyle = value; }
+
+    get strokeStyle() { return this.ctx.strokeStyle; }
+    set strokeStyle(value) { this.ctx.strokeStyle = value }
+
+    fillRect(){ return this.ctx.fillRect(...arguments); }
+    beginPath() { return this.ctx.beginPath(); }
+    stroke() { return this.ctx.stroke(); }
+    drawImage() {
+        //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+        if (arguments.length === 5){
+            return this.ctx.drawImage(
+                arguments[0],
+                arguments[1] * this.zoom,
+                arguments[2] * this.zoom,
+                arguments[3] * this.zoom,
+                arguments[4] * this.zoom
+            );
+        }
+        return this.ctx.drawImage(...arguments);
+    }
+    moveTo(x,y) {
+        //https://developer.mozilla.org/pt-BR/docs/Web/API/CanvasRenderingContext2D/moveTo
+        return this.ctx.moveTo(
+            x * this.zoom,
+            y * this.zoom
+        );
+    }
+    lineTo(x, y) {
+        //https://developer.mozilla.org/pt-BR/docs/Web/API/CanvasRenderingContext2D/lineTo
+        return this.ctx.lineTo(
+            x * this.zoom,
+            y * this.zoom
+        );
+    }
+
+    zoomIn(){
+        this.zoom += 0.05;
+    }
+    zoomOut(){
+        this.zoom -= 0.05;
+    }
+
+    drawBackground(sqr) {
+        this.fillStyle = "#FFFDF7";
+        this.fillRect(0, 0, w, h);
+
+        this.beginPath();
+        for (let i = 0; i < this.w / sqr; i++) {
+            this.moveTo(sqr * i, 0     );
+            this.lineTo(sqr * i, this.h);
+        }
+        for (let i = 0; i < this.h / sqr; i++) {
+            this.moveTo(0     , sqr * i);
+            this.lineTo(this.w, sqr * i);
+        }
+        this.strokeStyle = "#FFEECE";
+        this.stroke();
+    }
+};
---
+++ map.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let map = document.getElementById("map");
+let ctx = new My2dContext(map.getContext("2d"));
+
+let w = map.width = window.innerWidth;
+let h = map.height = window.innerHeight -4; //-4 pq sim
+
+let img = new Image();
+img.src = "img2.png";
+img.onload = () => ctx.drawImage(img, 0, 0, 100, 100);
+ctx.drawBackground(50);
+
+ctx.drawFunction = function(){
+    ctx.drawBackground(50);
+    ctx.drawImage(img, 0, 0, 100, 100);
+}
