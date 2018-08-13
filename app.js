08f539c1b60e723034d0e7d227e68fcb6298b1ff
Vinícius Hoyer <vhoyer@live.com>
2018-08-13 20:20:25 -0300

    change the grid size from 50 to 200

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ let ctx = new My2dContext(map.getContext("2d"));
 let w = map.width = window.innerWidth;
 let h = map.height = window.innerHeight -4; //-4 pq sim
 
+ctx.draw = function(){
+    ctx.drawBackground(200);
+    ctx.drawImage(img, 0, 0, 100, 100);
+
+    //debug
+}
+
 let img = new Image();
 img.src = "img2.png";
-img.onload = () => ctx.drawImage(img, 0, 0, 100, 100);
-ctx.drawBackground(50);
-
-ctx.draw = function(){
-    ctx.drawBackground(50);
-    ctx.drawImage(img, 0, 0, 100, 100);
-}
+img.onload = () => ctx.draw();
